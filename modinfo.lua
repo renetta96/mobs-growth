88d2655c1d72c0863f9ff6a94727ce5a91a0c94f
Hai Hoang Nguyen <hainh26121996@gmail.com>
2021-07-24 21:00:03 +0700

    Init

---
+++ modinfo.lua
@@ -0,0 +1,125 @@
+-- This information tells other players more about the mod
+name = "Mobs Growth"
+description = ""
+author = "Zeta"
+version = "1.0.0" -- This is the version of the template. Change it to your own number.
+
+-- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
+forumthread = ""
+
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+-- Compatible with Don't Starve Together
+dst_compatible = true
+
+-- Not compatible with Don't Starve
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+
+-- Character mods need this set to true
+all_clients_require_mod = true
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+-- The mod's tags displayed on the server list
+server_filter_tags = {
+	"tweaks",
+	"hardcore",
+	"growth"
+}
+
+configuration_options = {
+	{
+		name = 'GROWTH_INTERVAL',
+		label = 'Growth interval',
+		options = {
+			{ description = '10 days', data = 10 },
+			{ description = '20 days', data = 20 },
+			{ description = '30 days', data = 30 },
+			{ description = '40 days', data = 40 },
+			{ description = '50 days', data = 50 },
+			{ description = '60 days', data = 60 },
+			{ description = '70 days', data = 70 },
+			{ description = '80 days', data = 80 },
+			{ description = '90 days', data = 90 },
+			{ description = '100 days', data = 100 },
+		},
+		default = 50,
+		hover = 'How many days per growth'
+	},
+	{
+		name = 'MOB_HEALTH_GROWTH_RATE',
+		label = 'Mob health growth rate',
+		options = {
+			{ description = '10%', data = 0.1 },
+			{ description = '20%', data = 0.2 },
+			{ description = '30%', data = 0.3 },
+			{ description = '40%', data = 0.4 },
+			{ description = '50%', data = 0.5 },
+			{ description = '60%', data = 0.6 },
+			{ description = '70%', data = 0.7 },
+			{ description = '80%', data = 0.8 },
+			{ description = '90%', data = 0.9 },
+			{ description = '100%', data = 1 },
+		},
+		default = 0.3,
+		hover = 'How much health mobs grow each interval'
+	},
+	{
+		name = 'MOB_DAMAGE_GROWTH_RATE',
+		label = 'Mob damage growth rate',
+		options = {
+			{ description = '10%', data = 0.1 },
+			{ description = '20%', data = 0.2 },
+			{ description = '30%', data = 0.3 },
+			{ description = '40%', data = 0.4 },
+			{ description = '50%', data = 0.5 },
+			{ description = '60%', data = 0.6 },
+			{ description = '70%', data = 0.7 },
+			{ description = '80%', data = 0.8 },
+			{ description = '90%', data = 0.9 },
+			{ description = '100%', data = 1 },
+		},
+		default = 0.2,
+		hover = 'How much damage mobs grow each interval'
+	},
+	{
+		name = 'BOSS_HEALTH_GROWTH_RATE',
+		label = 'Boss health growth rate',
+		options = {
+			{ description = '10%', data = 0.1 },
+			{ description = '20%', data = 0.2 },
+			{ description = '30%', data = 0.3 },
+			{ description = '40%', data = 0.4 },
+			{ description = '50%', data = 0.5 },
+			{ description = '60%', data = 0.6 },
+			{ description = '70%', data = 0.7 },
+			{ description = '80%', data = 0.8 },
+			{ description = '90%', data = 0.9 },
+			{ description = '100%', data = 1 },
+		},
+		default = 0.3,
+		hover = 'How much health bosses grow each interval'
+	},
+	{
+		name = 'BOSS_DAMAGE_GROWTH_RATE',
+		label = 'Boss damage growth rate',
+		options = {
+			{ description = '10%', data = 0.1 },
+			{ description = '20%', data = 0.2 },
+			{ description = '30%', data = 0.3 },
+			{ description = '40%', data = 0.4 },
+			{ description = '50%', data = 0.5 },
+			{ description = '60%', data = 0.6 },
+			{ description = '70%', data = 0.7 },
+			{ description = '80%', data = 0.8 },
+			{ description = '90%', data = 0.9 },
+			{ description = '100%', data = 1 },
+		},
+		default = 0.2,
+		hover = 'How much damage bosses grow each interval'
+	},
+}
---
+++ modmain.lua
@@ -0,0 +1,125 @@
+local require = GLOBAL.require
+local math = GLOBAL.math
+
+local growth_interval = GetModConfigData('GROWTH_INTERVAL')
+local mob_health_grow_rate = GetModConfigData('MOB_HEALTH_GROWTH_RATE')
+local mob_damage_grow_rate = GetModConfigData('MOB_DAMAGE_GROWTH_RATE')
+local boss_health_grow_rate = GetModConfigData('BOSS_HEALTH_GROWTH_RATE')
+local boss_damage_grow_rate = GetModConfigData('BOSS_DAMAGE_GROWTH_RATE')
+
+local function HasLeader(inst)
+	return inst.components.follower
+		and inst.components.follower.leader
+		and inst.components.follower.leader:IsValid()
+		and inst.components.follower.leader:HasTag("player")
+end
+
+local function Grow(inst)
+	local cycles = GLOBAL.TheWorld.state.cycles
+  	local total_growths = math.floor(cycles / growth_interval)
+
+  	local health_rate = mob_health_grow_rate
+  	local damage_rate = mob_damage_grow_rate
+
+  	if inst:HasTag("epic") then
+  		health_rate = boss_health_grow_rate
+  		damage_rate = boss_damage_grow_rate
+  	end
+
+  	if HasLeader(inst) then
+  		total_growths = math.floor(total_growths / 2)
+  	end
+
+  	local growths_left = total_growths - inst.__growths
+
+	if inst.__origin_maxhealth ~= nil and inst.__growths ~= nil then
+		local delta_health = health_rate * growths_left * inst.__origin_maxhealth
+
+		print("DELTA HEALTH ", inst, delta_health)
+		local current_percent = inst.components.health:GetPercent()
+		inst.components.health.maxhealth = math.max(
+			inst.__origin_maxhealth,
+			inst.components.health.maxhealth + delta_health
+		)
+		inst.components.health.currenthealth = current_percent * inst.components.health.maxhealth
+		inst.__growths = total_growths
+	end
+
+	if inst.__origin_damagemultipler then
+		local delta_multiplier = damage_rate * growths_left * inst.__origin_damagemultipler
+
+		print("DELTA DAMAGE MULT ", inst, delta_multiplier)
+		local basemultiplier = inst.components.combat.damagemultiplier or 1
+		inst.components.combat.damagemultiplier =
+			math.max(inst.__origin_damagemultipler, basemultiplier + delta_multiplier)
+	end
+
+	if inst.__origin_areahitdamagepercent ~= nil and inst.components.combat.areahitdamagepercent ~= nil then
+		local delta_multiplier = damage_rate * growths_left * inst.__origin_areahitdamagepercent
+
+		print("DELTA AREA DAMAGE MULT ", inst, delta_multiplier)
+		inst.components.combat.areahitdamagepercent = math.max(
+			inst.__origin_areahitdamagepercent,
+			inst.components.combat.areahitdamagepercent + delta_multiplier
+		)
+	end
+end
+
+local function OnCyclesChanged(inst, cycles)
+	Grow(inst)
+end
+
+local function OnStopFollowing(inst)
+	print("STOP FOLLOWING")
+	Grow(inst)
+end
+
+local function OnStartFollowing(inst)
+	print("START FOLLOWING")
+	Grow(inst)
+end
+
+local blacklist = {
+	abigail = true
+}
+
+local function MakePrefabGrowth(inst)
+	if not GLOBAL.TheWorld.ismastersim then
+    	return
+  	end
+
+  	if inst:HasTag("player") then
+  		return
+  	end
+
+  	if blacklist[inst.prefab] then
+  		return
+  	end
+
+  	if not(
+  		inst:HasTag("monster")
+  		or inst:HasTag("animal")
+  		or inst:HasTag("character")
+  		or inst:HasTag("insect")
+  	) then
+  		return
+  	end
+
+  	if not(inst.components.combat and inst.components.health) then
+  		return
+  	end
+
+  	inst.__growths = 0
+  	inst.__origin_maxhealth = inst.components.health.maxhealth
+  	inst.__origin_damagemultipler = inst.components.combat.damagemultiplier or 1
+  	inst.__origin_areahitdamagepercent = inst.components.combat.areahitdamagepercent
+
+	Grow(inst)
+
+	inst:WatchWorldState("cycles", OnCyclesChanged)
+
+	inst:ListenForEvent("stopfollowing", OnStopFollowing)
+	inst:ListenForEvent("startfollowing", OnStartFollowing)
+end
+
+AddPrefabPostInitAny(MakePrefabGrowth)
