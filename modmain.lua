7065c4929bd6dc72360d880a00b5bc94156f23af
Hai Hoang Nguyen <hainh26121996@gmail.com>
2021-08-01 12:03:34 +0700

    handle change max health

--- modmain.lua
+++ modmain.lua
@@ -15,7 +15,8 @@ local boss_damage_grow_cap = GetModConfigData('BOSS_DAMAGE_GROWTH_CAP')
 
 local follower_grow_percent = GetModConfigData('FOLLOWER_GROWTH_PERCENTAGE')
 
-local follower_list = {
+local follower_list = {}
+local exception_list = {
 	abigail = true
 }
 
@@ -27,6 +28,7 @@ local function HasLeader(inst)
 end
 
 local function Grow(inst)
+	-- print("GROW")
 	if not inst:IsValid() then
 		return
 	end
@@ -46,26 +48,27 @@ local function Grow(inst)
   		damage_cap = boss_damage_grow_cap
   	end
 
-  	if follower_list[inst.prefab] or HasLeader(inst) then
+  	if (follower_list[inst.prefab] or HasLeader(inst)) and not exception_list[inst.prefab] then
   		total_growths = math.floor(total_growths * follower_grow_percent)
   		health_cap = health_cap * follower_grow_percent
   		damage_cap = damage_cap * follower_grow_percent
   	end
 
-  	local growths_left = total_growths - inst.__growths
+  	local growths_health_left = total_growths - inst.__growths_health
+  	local growths_damage_left = total_growths - inst.__growths_damage
 
-  	local delta_health_rate = health_rate * growths_left
+  	local delta_health_rate = health_rate * growths_health_left
   	if health_cap >= 0 then
-  		delta_health_rate = math.min(delta_health_rate, math.max(0, health_cap - inst.__growths * health_rate))
+  		delta_health_rate = math.min(delta_health_rate, math.max(0, health_cap - inst.__growths_health * health_rate))
   	end
 
-  	local delta_damage_rate = damage_rate * growths_left
+  	local delta_damage_rate = damage_rate * growths_damage_left
   	if damage_cap >= 0 then
-  		delta_damage_rate = math.min(delta_damage_rate, math.max(0, damage_cap - inst.__growths * damage_rate))
+  		delta_damage_rate = math.min(delta_damage_rate, math.max(0, damage_cap - inst.__growths_damage * damage_rate))
   	end
 
-  	if inst.components.health then
-  		if inst.__origin_maxhealth ~= nil and inst.__growths ~= nil then
+  	if inst.components.health and inst.__growths_health ~= nil then
+  		if inst.__origin_maxhealth ~= nil then
 			local delta_health = delta_health_rate * inst.__origin_maxhealth
 
 			-- print("DELTA HEALTH ", inst, delta_health)
@@ -74,12 +77,13 @@ local function Grow(inst)
 				inst.__origin_maxhealth,
 				inst.components.health.maxhealth + delta_health
 			)
-			inst.components.health.currenthealth = current_percent * inst.components.health.maxhealth
-			inst.__growths = total_growths
+
+			inst.components.health:SetPercent(current_percent)
+			inst.__growths_health = total_growths
 		end
   	end
 
-  	if inst.components.combat then
+  	if inst.components.combat and inst.__growths_damage ~= nil then
   		if inst.__origin_damagemultipler then
 			local delta_multiplier = delta_damage_rate * inst.__origin_damagemultipler
 
@@ -98,6 +102,8 @@ local function Grow(inst)
 				inst.components.combat.areahitdamagepercent + delta_multiplier
 			)
 		end
+
+		inst.__growths_damage = total_growths
   	end
 end
 
@@ -126,6 +132,10 @@ local function MakePrefabGrowth(inst)
   		return
   	end
 
+  	if inst:HasTag("shadow") then
+  		return
+  	end
+
   	if blacklist[inst.prefab] then
   		return
   	end
@@ -143,15 +153,30 @@ local function MakePrefabGrowth(inst)
   		return
   	end
 
-  	inst.__growths = 0
+  	inst.__growths_health = 0
+  	inst.__growths_damage = 0
   	inst.__origin_maxhealth = inst.components.health.maxhealth
   	inst.__origin_damagemultipler = inst.components.combat.damagemultiplier or 1
   	inst.__origin_areahitdamagepercent = inst.components.combat.areahitdamagepercent
 
+  	-- print("ORIGIN MAX HEALTH ", inst.__origin_maxhealth)
+
 	Grow(inst)
 
-	inst:WatchWorldState("cycles", OnCyclesChanged)
+	local oldSetMaxHealth = inst.components.health.SetMaxHealth
 
+	inst.components.health.SetMaxHealth = function(comp, amount, ...)
+		oldSetMaxHealth(comp, amount, ...)
+
+		-- print("SET MAX HEALTH ", amount)
+
+		-- Reset health growth
+		inst.__origin_maxhealth = amount
+		inst.__growths_health = 0
+		Grow(inst)
+	end
+
+	inst:WatchWorldState("cycles", OnCyclesChanged)
 	inst:ListenForEvent("stopfollowing", OnStopFollowing)
 	inst:ListenForEvent("startfollowing", OnStartFollowing)
 end
